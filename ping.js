ffd91b1629d599a8c42a6563c6c572e98886aabd
Adam Fortuna <adam@fortuna.name>
2017-02-28 14:24:17 -0500

    Added ability to ping back with a unique identifier

--- ping.js
+++ ping.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
 var stdin = process.openStdin();
+
+try {
+  var uid = fs.readFileSync('./.projects_config', 'utf8');
+} catch(e) {
+  console.log("\n======\nMake sure to download the '.projects_config' file from the project page on Pluralsight and add it to the root directory of this project then re-run this command.\n======\n\n");
+  process.exit(1);
+}
 
 stdin.resume();
 stdin.setEncoding('utf8');
@@ -11,7 +19,7 @@ stdin.on('data', function(report) {
     var parsedData = JSON.parse(report);
 
     // Send the data over to the Projects application
-    console.log("Todo: Send report to Pluralsight.")
+    console.log("Todo: Send report to Pluralsight:", uid)
   } catch(e) {
     // Skip doing anything with this result
   }
